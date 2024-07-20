278c0ce3b0ec92047a609a862e55b6b2faa7d651
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-21 01:33:26 +0500

    chap 16 done

---
+++ index.js
@@ -0,0 +1,48 @@
+chap no 16 adding and removing 
+
+const books=["maths","physics","chemisstry"];
+console.log(books);
+sigle element add 
+push element this meane add and another element and the end of the array 
+books.push("physics");
+books.push("maths");
+books.push("biology");
+books.push("pashtoo");
+books.push("ict");
+books.push("maths");
+console.log(books);
+
+
+
+const burger=["checken","bred","keechap"];
+burger.push("jamburger","");
+burger.push("millkburger","goolden");
+console.log(burger);
+
+replacing oin element  for specific
+const books=["computescience","maths"];
+// books[1]= "physics";
+console.log(books);
+maths ki jaga ab physics agia 
+
+adding element to empty array 
+const books=[];
+books[0]="physics";
+books[1]="maths";
+books[2]="computer science";
+books[3]="ict";
+books[10]="islamiat";
+console.log(books);
+
+removed in element pop 
+const books=["urdu","english","islamaiat"];
+books.pop();
+books.pop();
+console.log(books);
+
+// pop is removed in last element
+// again include in subject
+const poppedBook= books.pop();
+console.log(books);
+console.log({poppedBook};)
+
